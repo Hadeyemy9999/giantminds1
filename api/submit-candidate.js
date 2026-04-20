b056f06f30e9dd99d576840a283d33c7fd0104e7
Hadeyemy9999 <codewriter0206@gmail.com>
2026-04-20 21:01:23 +0100

    fix: update formidable import syntax for v3 compatibility

--- api/submit-candidate.js
+++ api/submit-candidate.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const formidable = require('formidable');
+const { formidable } = require('formidable');
 const { sendCandidateEmail } = require('../services/emailService');
 
 function parseForm(req) {
--- api/submit-partner.js
+++ api/submit-partner.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const formidable = require('formidable');
+const { formidable } = require('formidable');
 const { sendPartnerEmail } = require('../services/emailService');
 
 function parseForm(req) {
